3fd604b9e3629badadc1b0605248f3ffecec591c
Alexandre Bérard <eskaev@gmail.com>
2016-03-17 17:16:42 +0100

    Fixed import

--- rnn/translate/translate.py
+++ rnn/translate/translate.py
@@ -41,8 +41,8 @@ import numpy as np
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-import data_utils
-from tensorflow.models.rnn.translate import seq2seq_model
+from rnn.translate import seq2seq_model
+from rnn.translate import data_utils
 
 
 tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
