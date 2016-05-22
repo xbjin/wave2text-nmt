7d2830d0b56b129cd775203932851aba51be4d93
Alexandre Bérard <eskaev@gmail.com>
2016-05-22 21:39:19 +0200

    Fixed small bug

--- scripts/experiments/simulated-PE/prepare-Potet.sh
+++ scripts/experiments/simulated-PE/prepare-Potet.sh
@@ -11,9 +11,9 @@ mkdir -p ${data_dir}
 
 # we assume that PEofMT* and PEofREF* are in data dir
 
-cp ${data_dir}/PEofMT-10881.pe ${corpus}.en
-cp ${data_dir}/PEofMT-10881.mt ${corpus}.mt
-cp ${data_dir}/PEofMT-10881.fr ${corpus}.fr
+cp ${data_dir}/PEofMT-10881.pe ${data_dir}/${corpus}.en
+cp ${data_dir}/PEofMT-10881.mt ${data_dir}/${corpus}.mt
+cp ${data_dir}/PEofMT-10881.fr ${data_dir}/${corpus}.fr
 
 # notice the "-test" (output will be ".test")
 cp ${data_dir}/PEofREF-1500.pe-mt ${data_dir}/${corpus}-test.en
@@ -24,7 +24,7 @@ cp ${data_dir}/PEofREF-1500.fr ${data_dir}/${corpus}-test.fr
 ${script_dir}/prepare-data.py ${data_dir}/${corpus} fr mt en ${data_dir} --output-prefix ${corpus} \
   --mode prepare \
   --verbose \
-  --test-corpus ${corpus}-test \
+  --test-corpus ${data_dir}/${corpus}-test \
   --dev-size 800 \
   --normalize-digits \
   --normalize-punk \
