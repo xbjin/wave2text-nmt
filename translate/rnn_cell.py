aec5be28844bcc5c595d1ff425d71ada9d8b40d9
jbdel <jeanbenoit.delbrouck@gmail.com>
2016-06-07 14:37:24 +0200

    debug

--- translate/rnn_cell.py
+++ translate/rnn_cell.py
@@ -23,7 +23,7 @@ import math
 # pylint: disable=redefined-builtin,unused-import
 from six.moves import xrange
 # pylint: enable=redefined-builtin,unused-import
-
+from translate import utils
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import clip_ops
@@ -722,10 +722,10 @@ class EmbeddingWrapper(RNNCell):
     """Run the cell on embedded inputs."""
     with vs.variable_scope(scope or type(self).__name__):  # "EmbeddingWrapper"
       with ops.device("/cpu:0"):
-        print("initilaizer", initializer)
-        print("init type", type(initializer))
-        print("init self", self._initializer)
-        print("init self type", type(self._initializer))
+        utils.log("initilaizer", initializer)
+        utils.log("init type", type(initializer))
+        utils.log("init self", self._initializer)
+        utils.log("init self type", type(self._initializer))
         if self._initializer:
           initializer = self._initializer
         elif vs.get_variable_scope().initializer:
