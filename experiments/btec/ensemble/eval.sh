6c668ca9a4553efcff5689497a8ab92891f6244f
eske <eskaev@gmail.com>
2016-10-14 12:53:50 +0200

    Fixed BTEC eval script

--- experiments/btec/ensemble/eval.sh
+++ experiments/btec/ensemble/eval.sh
@@ -1,56 +1,30 @@
 #!/bin/bash
 
-config=experiments/speech_translation/ensemble/model_1.yaml
-ensemble_dir=experiments/speech_translation/ensemble
+config=experiments/btec/ensemble/model_1.yaml
+ensemble_dir=experiments/btec/ensemble
 model_dir=${ensemble_dir}/model_1
-data_dir=experiments/speech/data
+data_dir=experiments/btec/data
 
-#python2 -m translate ${config} --decode ${data_dir}/dev.Agnes -v --output ${model_dir}/dev.Agnes.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/dev.Michel -v --output ${model_dir}/dev.Michel.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/dev.Marion -v --output ${model_dir}/dev.Marion.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/test1.Agnes -v --output ${model_dir}/test1.Agnes.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/test1.Michel -v --output ${model_dir}/test1.Michel.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/test1.Marion -v --output ${model_dir}/test1.Marion.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes -v --output ${model_dir}/train.1000.Agnes.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel -v --output ${model_dir}/train.1000.Michel.greedy.best
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Marion -v --output ${model_dir}/train.1000.Marion.greedy.best
+parameters="-v"
+python2 -m translate ${config} --decode ${data_dir}/dev --output ${ensemble_dir}/dev.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1 --output ${ensemble_dir}/test1.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2 --output ${ensemble_dir}/test2.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000 --output ${ensemble_dir}/train.1000.greedy.out ${parameters}
 
-#python2 -m translate ${config} --decode ${data_dir}/dev.Agnes -v --output ${model_dir}/dev.Agnes.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/dev.Michel -v --output ${model_dir}/dev.Michel.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/dev.Marion -v --output ${model_dir}/dev.Marion.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/test1.Agnes -v --output ${model_dir}/test1.Agnes.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/test1.Michel -v --output ${model_dir}/test1.Michel.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/test1.Marion -v --output ${model_dir}/test1.Marion.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes -v --output ${model_dir}/train.1000.Agnes.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel -v --output ${model_dir}/train.1000.Michel.beam8.best --beam-size 8
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Marion -v --output ${model_dir}/train.1000.Marion.beam8.best --beam-size 8
+parameters="-v --beam-size 8"
+python2 -m translate ${config} --decode ${data_dir}/dev --output ${ensemble_dir}/dev.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1 --output ${ensemble_dir}/test1.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2 --output ${ensemble_dir}/test2.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000 --output ${ensemble_dir}/train.1000.beam8.out ${parameters}
 
-#python2 -m translate ${config} --decode ${data_dir}/dev.Agnes -v --output ${model_dir}/dev.Agnes.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/dev.Michel -v --output ${model_dir}/dev.Michel.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/dev.Marion -v --output ${model_dir}/dev.Marion.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/test1.Agnes -v --output ${model_dir}/test1.Agnes.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/test1.Michel -v --output ${model_dir}/test1.Michel.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/test1.Marion -v --output ${model_dir}/test1.Marion.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes -v --output ${model_dir}/train.1000.Agnes.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel -v --output ${model_dir}/train.1000.Michel.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-#python2 -m translate ${config} --decode ${data_dir}/train.1000.Marion -v --output ${model_dir}/train.1000.Marion.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
+parameters="-v --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2"
+python2 -m translate ${config} --decode ${data_dir}/dev --output ${ensemble_dir}/dev.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1 --output ${ensemble_dir}/test1.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2 --output ${ensemble_dir}/test2.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000 --output ${ensemble_dir}/train.1000.beam8.lm20.out ${parameters}
 
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/dev.Agnes -v --output ${ensemble_dir}/dev.Agnes.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/dev.Michel -v --output ${ensemble_dir}/dev.Michel.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/dev.Marion -v --output ${ensemble_dir}/dev.Marion.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/test1.Agnes -v --output ${ensemble_dir}/test1.Agnes.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/test1.Michel -v --output ${ensemble_dir}/test1.Michel.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/test1.Marion -v --output ${ensemble_dir}/test1.Marion.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/train.1000.Agnes -v --output ${ensemble_dir}/train.1000.Agnes.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/train.1000.Michel -v --output ${ensemble_dir}/train.1000.Michel.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best --ensemble --decode ${data_dir}/train.1000.Marion -v --output ${ensemble_dir}/train.1000.Marion.ensemble.beam8.lm20.best --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2
-
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/dev.Agnes -v --output ${ensemble_dir}/dev.Agnes.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/dev.Michel -v --output ${ensemble_dir}/dev.Michel.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/dev.Marion -v --output ${ensemble_dir}/dev.Marion.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/test1.Agnes -v --output ${ensemble_dir}/test1.Agnes.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/test1.Michel -v --output ${ensemble_dir}/test1.Michel.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/test1.Marion -v --output ${ensemble_dir}/test1.Marion.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/train.1000.Agnes -v --output ${ensemble_dir}/train.1000.Agnes.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/train.1000.Michel -v --output ${ensemble_dir}/train.1000.Michel.ensemble3.beam8.best --beam-size 8 
-#python2 -m translate ${config} --checkpoints ${ensemble_dir}/model_{1,3,5}/checkpoints/best --decode ${data_dir}/train.1000.Marion -v --output ${ensemble_dir}/train.1000.Marion.ensemble3.beam8.best --beam-size 8 
+parameters="-v --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2 --ensemble --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best"
+python2 -m translate ${config} --decode ${data_dir}/dev --output ${ensemble_dir}/dev.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1 -v --output ${ensemble_dir}/test1.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2 -v --output ${ensemble_dir}/test2.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000 -v --output ${ensemble_dir}/train.1000.beam8.lm20.ensemble.out ${parameters}
--- experiments/speech_translation/ensemble/eval.sh
+++ experiments/speech_translation/ensemble/eval.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-config=experiments/btec/ensemble/model_1.yaml
-ensemble_dir=experiments/btec/ensemble
+config=experiments/speech_translation/ensemble/model_1.yaml
+ensemble_dir=experiments/speech_translation/ensemble
 model_dir=${ensemble_dir}/model_1
-data_dir=experiments/btec/data
+data_dir=experiments/speech/data
 
 parameters="-v"
-python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${model_dir}/dev.Agnes.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Agnes --output ${model_dir}/test1.Agnes.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Agnes --output ${model_dir}/test2.Agnes.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes --output ${model_dir}/train.1000.Agnes.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${model_dir}/dev.Michel.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Michel --output ${model_dir}/test1.Michel.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Michel --output ${model_dir}/test2.Michel.greedy.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel --output ${model_dir}/train.1000.Michel.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${ensemble_dir}/dev.Agnes.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Agnes --output ${ensemble_dir}/test1.Agnes.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Agnes --output ${ensemble_dir}/test2.Agnes.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes --output ${ensemble_dir}/train.1000.Agnes.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${ensemble_dir}/dev.Michel.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Michel --output ${ensemble_dir}/test1.Michel.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Michel --output ${ensemble_dir}/test2.Michel.greedy.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel --output ${ensemble_dir}/train.1000.Michel.greedy.out ${parameters}
 
 parameters="-v --beam-size 8"
-python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${model_dir}/dev.Agnes.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Agnes --output ${model_dir}/test1.Agnes.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Agnes --output ${model_dir}/test2.Agnes.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes --output ${model_dir}/train.1000.Agnes.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${model_dir}/dev.Michel.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Michel --output ${model_dir}/test1.Michel.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Michel --output ${model_dir}/test2.Michel.beam8.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel --output ${model_dir}/train.1000.Michel.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${ensemble_dir}/dev.Agnes.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Agnes --output ${ensemble_dir}/test1.Agnes.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Agnes --output ${ensemble_dir}/test2.Agnes.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes --output ${ensemble_dir}/train.1000.Agnes.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${ensemble_dir}/dev.Michel.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Michel --output ${ensemble_dir}/test1.Michel.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Michel --output ${ensemble_dir}/test2.Michel.beam8.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel --output ${ensemble_dir}/train.1000.Michel.beam8.out ${parameters}
 
 parameters="-v --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2"
-python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${model_dir}/dev.Agnes.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Agnes --output ${model_dir}/test1.Agnes.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Agnes --output ${model_dir}/test2.Agnes.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes --output ${model_dir}/train.1000.Agnes.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${model_dir}/dev.Michel.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Michel --output ${model_dir}/test1.Michel.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Michel --output ${model_dir}/test2.Michel.beam8.lm20.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel --output ${model_dir}/train.1000.Michel.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${ensemble_dir}/dev.Agnes.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Agnes --output ${ensemble_dir}/test1.Agnes.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Agnes --output ${ensemble_dir}/test2.Agnes.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes --output ${ensemble_dir}/train.1000.Agnes.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${ensemble_dir}/dev.Michel.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Michel --output ${ensemble_dir}/test1.Michel.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Michel --output ${ensemble_dir}/test2.Michel.beam8.lm20.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel --output ${ensemble_dir}/train.1000.Michel.beam8.lm20.out ${parameters}
 
 parameters="-v --beam-size 8 --lm-file ${data_dir}/btec.arpa --lm-weight 0.2 --ensemble --checkpoints ${ensemble_dir}/model_{1,2,3,4,5}/checkpoints/best"
-python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${model_dir}/dev.Agnes.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Agnes -v --output ${model_dir}/test1.Agnes.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Agnes -v --output ${model_dir}/test2.Agnes.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes -v --output ${model_dir}/train.1000.Agnes.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${model_dir}/dev.Michel.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test1.Michel -v --output ${model_dir}/test1.Michel.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/test2.Michel -v --output ${model_dir}/test2.Michel.beam8.lm20.ensemble.out ${parameters}
-python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel -v --output ${model_dir}/train.1000.Michel.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Agnes --output ${ensemble_dir}/dev.Agnes.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Agnes -v --output ${ensemble_dir}/test1.Agnes.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Agnes -v --output ${ensemble_dir}/test2.Agnes.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Agnes -v --output ${ensemble_dir}/train.1000.Agnes.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/dev.Michel --output ${ensemble_dir}/dev.Michel.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test1.Michel -v --output ${ensemble_dir}/test1.Michel.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/test2.Michel -v --output ${ensemble_dir}/test2.Michel.beam8.lm20.ensemble.out ${parameters}
+python2 -m translate ${config} --decode ${data_dir}/train.1000.Michel -v --output ${ensemble_dir}/train.1000.Michel.beam8.lm20.ensemble.out ${parameters}
