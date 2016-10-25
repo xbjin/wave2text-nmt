21eb71b3e9edfe71df0c52f5a991eebbbcd1fac0
Loïc Vial <contact.coyl@gmail.com>
2016-10-25 18:54:26 +0200

    Create a SemCor corpus without the "only verbs" part

--- wsd/generate_data.sh
+++ wsd/generate_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
 cd ..
-./scripts/prepare-data.py wsd/corpus/semcor lem tag wsd/data/ --dev-size 1000 --test-size 1000 --no-tokenize --shuffle --vocab-size 0
+./scripts/prepare-data.py wsd/corpus/semcor12 lem tag wsd/data/ --dev-size 1000 --test-size 1000 --no-tokenize --shuffle --vocab-size 0
 
