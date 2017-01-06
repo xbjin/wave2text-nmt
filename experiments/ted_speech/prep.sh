d5f3390655908153dda55b4e2572703c366b7103
chengzhu <cyu0913@gmail.com>
2017-01-06 10:11:45 -0600

    updated ted_speech/prep.sh to save features for each file

--- experiments/ted_speech/prep.sh
+++ experiments/ted_speech/prep.sh
@@ -13,10 +13,16 @@ mkdir -p $data_mfcc_train $data_mfcc_dev
 while read line
 do
    uttname=`basename $line .wav`
+   feats=$data_mfcc_train/${uttname}.feats41 	
+   scripts/extract-audio-features.py $line --output=$feats
+done < $raw_audio_train
+
+while read line
+do
+   uttname=`basename $line .wav`
    feats=$data_mfcc_dev/${uttname}.feats41 	
-   #scripts/extract-audio-features.py $raw_audio_train --output=$data_mfcc_train
-   scripts/extract-audio-features.py $raw_audio_dev --output=$feats
+   scripts/extract-audio-features.py $line --output=$feats
 done < $raw_audio_dev
-scripts/extract-audio-features.py $raw_audio_dev --output=$data_mfcc_dev
-#scripts/prepare-data.py $raw_txt_train_prefix en $data_txt_dir --max 0 --lowercase --output train --mode all
-#scripts/prepare-data.py $raw_txt_dev_prefix en $data_txt_dir --max 0 --lowercase --output dev --mode prepare
+
+scripts/prepare-data.py $raw_txt_train_prefix en $data_txt_dir --max 0 --lowercase --output train --mode all
+scripts/prepare-data.py $raw_txt_dev_prefix en $data_txt_dir --max 0 --lowercase --output dev --mode prepare
